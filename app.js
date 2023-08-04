f1ac8be36af904a7a920b12b1f47aa6ac018b713
islam amr <islam.mohamed02@eng-st.cu.edu.eg>
2023-08-04 19:26:33 +0300

    Using bolierplates and adding styles using bootstrap

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 mongoose.set('strictQuery',true);
@@ -15,6 +16,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine',  'ejs');
 app.set('views', path.join(__dirname,'views'));
 
