50858bf23531814d71294d2d1eac0fbb1de234bf
islam amr <islam.mohamed02@eng-st.cu.edu.eg>
2023-08-23 00:55:07 +0300

    Added image upload

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,16 +3,19 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const {isLoggedIn, isAuthor,validateCampground} = require('../middleware');
 const campgrounds = require('../controllers/campgrounds');
+const multer = require('multer');
+const {storage} = require('../cloudinary')
+const upload = multer({storage});
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn('create a campground'), validateCampground, catchAsync(campgrounds.createCampground));
+    .post(isLoggedIn('create a campground'), upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
 
 router.get('/new', isLoggedIn('create a campground'), campgrounds.renderNewForm);
 
 router.route('/:id')
     .get(catchAsync(campgrounds.showCampground))
-    .put(isLoggedIn('edit a campground'), isAuthor('edit'), validateCampground, catchAsync(campgrounds.updateCampground))
+    .put(isLoggedIn('edit a campground'), isAuthor('edit'), upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
     .delete (isLoggedIn('delete a campground'), isAuthor('delete'), catchAsync(campgrounds.deleteCampground));
 
 router.get('/:id/edit', isLoggedIn('edit a campground'), isAuthor('edit'), catchAsync(campgrounds.renderEditForm));
--- seeds/index.js
+++ seeds/index.js
@@ -24,9 +24,18 @@ const seedDB = async () => {
             author: '64e0eb31aac07a0e7995b17e',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://picsum.photos/640/360',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            price
+            price,
+            images:  [
+                {
+                  url: 'https://res.cloudinary.com/dojdrzsmy/image/upload/v1692568004/Yelp-Camp/nr3dlvi4e1a5drbipwnh.jpg',
+                  filename: 'Yelp-Camp/nr3dlvi4e1a5drbipwnh',
+                },
+                {
+                  url: 'https://res.cloudinary.com/dojdrzsmy/image/upload/v1692568004/Yelp-Camp/nzpnkjl8jaz4mho1gcrq.jpg',
+                  filename: 'Yelp-Camp/nzpnkjl8jaz4mho1gcrq',
+                }
+              ]
         })
         await camp.save();
     }
